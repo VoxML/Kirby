5a446dbd49da6ae104486d337890df98df29be1b
nkrishnaswamy <nkrishn87@gmail.com>
2020-04-01 17:13:46 -0400

    Add RedisSocket, rename scene

---
+++ Assets/Scripts/RedisIOClient.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using VoxSimPlatform.Network;
+
+public class RedisIOClient : MonoBehaviour
+{
+    RedisSocket _redisSocket;
+    public RedisSocket RedisSocket
+    {
+        get { return _redisSocket; }
+        set { _redisSocket = value; }
+    }
+
+    CommunicationsBridge commBridge;
+
+    // Use this for initialization
+    void Start()
+    {
+        commBridge = GameObject.Find("CommunicationsBridge").GetComponent<CommunicationsBridge>();
+        _redisSocket = (RedisSocket)commBridge.FindSocketConnectionByType(typeof(RedisIOClient));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_redisSocket != null)
+        {
+            string redisUrl = string.Format("{0}:{1}", _redisSocket.Address, _redisSocket.Port);
+            if (_redisSocket.IsConnected())
+            {
+                if (commBridge.tryAgainSockets.ContainsKey(redisUrl))
+                {
+                    if (commBridge.tryAgainSockets[redisUrl] == typeof(RedisSocket))
+                    {
+                        _redisSocket = (RedisSocket)commBridge.FindSocketConnectionByType(typeof(RedisIOClient));
+                    }
+                }
+
+                string inputFromRedis = _redisSocket.GetMessage();
+                if (inputFromRedis != "")
+                {
+                    Debug.Log(inputFromRedis);
+                    Debug.Log(_redisSocket.HowManyLeft() + " messages left.");
+                    _redisSocket.OnUpdateReceived(this, new RedisEventArgs(inputFromRedis));
+                }
+            }
+            else
+            {
+                if (!commBridge.tryAgainSockets.ContainsKey(redisUrl))
+                {
+                    commBridge.tryAgainSockets.Add(redisUrl, _redisSocket.GetType());
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RedisSocket.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System;
+
+using VoxSimPlatform.Global;
+using VoxSimPlatform.Network;
+
+public class RedisEventArgs : EventArgs
+{
+    public string Content { get; set; }
+
+    public RedisEventArgs(string content)
+    {
+        this.Content = content;
+    }
+}
+
+public class RedisSocket : SocketConnection
+{
+    public event EventHandler UpdateReceived;
+
+    public void OnUpdateReceived(object sender, EventArgs e)
+    {
+        if (UpdateReceived != null)
+        {
+            UpdateReceived(this, e);
+        }
+    }
+
+    public RedisSocket()
+    {
+        IOClientType = typeof(RedisIOClient);
+    }
+
+    public void Write(byte[] content)
+    {
+        // Check to see if this NetworkStream is writable.
+        if (_client.GetStream().CanWrite)
+        {
+            byte[] writeBuffer = content;
+            if (!BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(writeBuffer);
+            }
+
+            _client.GetStream().Write(writeBuffer, 0, writeBuffer.Length);
+            Debug.Log(string.Format("Written to this NetworkStream: {0} ({1})", writeBuffer.Length,
+                GlobalHelper.PrintByteArray(writeBuffer)));
+        }
+        else
+        {
+            Debug.Log("Sorry.  You cannot write to this NetworkStream.");
+        }
+    }
+}
