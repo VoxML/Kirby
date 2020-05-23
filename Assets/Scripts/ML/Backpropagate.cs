858d9f2d72457e1bfa9fd71faa885f0fdd0e5e9e
nkrishnaswamy <nkrishn87@gmail.com>
2020-05-23 15:01:42 -0400

    Add neural network framework and tests

---
+++ Assets/Scripts/ML/Backpropagate.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Backpropagate
+{
+    // Backpropagation implementation
+    // backpropagation
+    public static void BackPropagate(this NeuralNetwork nn, float[] inputs, float[] expected)
+    {
+        // runs feed forward to ensure neurons are populated correctly
+        float[] output = nn.FeedForward(inputs);
+
+        nn.cost = 0;
+
+        // calculated cost of network
+        for (int i = 0; i < output.Length; i++)
+        {
+            nn.cost += (float)Math.Pow(output[i] - expected[i], 2);
+        }
+
+        // this value is not used in calculions, rather used to identify the performance of the network
+        nn.cost = nn.cost / 2;
+
+        // gamma initialization
+        float[][] gamma;
+        List<float[]> gammaList = new List<float[]>();
+        for (int i = 0; i < nn.Layers.Length; i++)
+        {
+            gammaList.Add(new float[nn.Layers[i]]);
+        }
+        gamma = gammaList.ToArray();
+
+        int layer = nn.Layers.Length - 2;
+
+        // gamma calculation
+        for (int i = 0; i < output.Length; i++)
+        {
+            gamma[nn.Layers.Length - 1][i] = (output[i] - expected[i]) * activateDyDx(nn, output[i], layer);
+        }
+
+        // calculates the w' and b' for the last layer in the network
+        for (int i = 0; i < nn.Layers[nn.Layers.Length - 1]; i++)
+        {
+            nn.Biases[nn.Layers.Length - 2][i] -= gamma[nn.Layers.Length - 1][i] * nn.learningRate;
+            for (int j = 0; j < nn.Layers[nn.Layers.Length - 2]; j++)
+            {
+                //*learning 
+                nn.Weights[nn.Layers.Length - 2][i][j] -= gamma[nn.Layers.Length - 1][i] * nn.Neurons[nn.Layers.Length - 2][j] * nn.learningRate;
+            }
+        }
+
+        // runs on all hidden layers
+        for (int i = nn.Layers.Length - 2; i > 0; i--)
+        {
+            layer = i - 1;
+
+            // outputs
+            for (int j = 0; j < nn.Layers[i]; j++)
+            {
+                // calculate gamma
+                gamma[i][j] = 0;
+                for (int k = 0; k < gamma[i + 1].Length; k++)
+                {
+                    gamma[i][j] = gamma[i + 1][k] * nn.Weights[i][k][j];
+                }
+                gamma[i][j] *= activateDyDx(nn, nn.Neurons[i][j], layer);
+            }
+
+            // iterate over outputs of layer
+            for (int j = 0; j < nn.Layers[i]; j++)
+            {
+                // modify biases of network
+                nn.Biases[i - 1][j] -= gamma[i][j] * nn.learningRate;
+
+                // iterate over inputs to layer
+                for (int k = 0; k < nn.Layers[i - 1]; k++)
+                {
+                    // modify weights of network
+                    nn.Weights[i - 1][j][k] -= gamma[i][j] * nn.Neurons[i - 1][k] * nn.learningRate;
+                }
+            }
+        }
+    }
+
+    // all activation functions
+    public static float activate(NeuralNetwork nn, float value, int layer)
+    {
+        switch (nn.Activations[layer])
+        {
+            case Activation.Sigmoid:
+                return sigmoid(value);
+            case Activation.Tanh:
+                return tanh(value);
+            case Activation.ReLU:
+                return relu(value);
+            case Activation.LeakyReLU:
+                return leakyRelu(value);
+            default:
+                return relu(value);
+        }
+    }
+
+    // all activation function derivatives
+    public static float activateDyDx(NeuralNetwork nn, float value, int layer)
+    {
+        switch (nn.Activations[layer])
+        {
+            case Activation.Sigmoid:
+                return sigmoidDyDx(value);
+            case Activation.Tanh:
+                return tanhDyDx(value);
+            case Activation.ReLU:
+                return reluDyDx(value);
+            case Activation.LeakyReLU:
+                return leakyReluDyDx(value);
+            default:
+                return reluDyDx(value);
+        }
+    }
+
+    // activation functions and their corresponding derivatives
+    public static float sigmoid(float x)
+    {
+        float k = (float)Math.Exp(x);
+        return k / (1.0f + k);
+    }
+
+    public static float tanh(float x)
+    {
+        return (float)Math.Tanh(x);
+    }
+
+    public static float relu(float x)
+    {
+        return (0 >= x) ? 0 : x;
+    }
+
+    public static float leakyRelu(float x)
+    {
+        return (0 >= x) ? 0.01f * x : x;
+    }
+
+    public static float sigmoidDyDx(float x)
+    {
+        return x * (1 - x);
+    }
+
+    public static float tanhDyDx(float x)
+    {
+        return 1 - (x * x);
+    }
+
+    public static float reluDyDx(float x)
+    {
+        return (0 >= x) ? 0 : 1;
+    }
+
+    public static float leakyReluDyDx(float x)
+    {
+        return (0 >= x) ? 0.01f : 1;
+    }
+}
---
+++ Assets/Scripts/ML/NeuralNetwork.cs
@@ -0,0 +1,299 @@
+﻿/*
+ * Native Unity neural network implementation
+ * derived from https://github.com/kipgparker/BackPropNetwork
+ * TODO: if this works, move it to VoxSimPlatform
+ */
+
+using System.Collections.Generic;
+using System.IO;
+
+using VoxSimPlatform.Global;
+
+public enum Activation
+{
+    Sigmoid,
+    Tanh,
+    ReLU,
+    LeakyReLU
+}
+
+public class NeuralNetwork
+{
+    // fundamental 
+    private int[] layers;
+    public int[] Layers
+    {
+        get { return layers; }
+    }
+
+    private float[][] neurons;
+    public float[][] Neurons
+    {
+        get { return neurons; }
+    }
+
+    private float[][] biases;
+    public float[][] Biases
+    {
+        get { return biases; }
+    }
+
+    private float[][][] weights;
+    public float[][][] Weights
+    {
+        get { return weights; }
+    }
+
+    private Activation[] activations;
+    public Activation[] Activations
+    {
+        get { return activations; }
+    }
+
+    // genetic
+    public float fitness = 0;
+
+    // backprop
+    public float learningRate;
+    public float cost;
+
+    private float[][] deltaBiases;
+    private float[][][] deltaWeights;
+    private int deltaCount;
+
+    public NeuralNetwork(int[] layers, string[] layerActivations, float _learningRate, float _cost)
+    {
+        learningRate = _learningRate;
+        cost = _cost;
+
+        this.layers = new int[layers.Length];
+        for (int i = 0; i < layers.Length; i++)
+        {
+            this.layers[i] = layers[i];
+        }
+
+        activations = new Activation[layers.Length - 1];
+        for (int i = 0; i < layers.Length - 1; i++)
+        {
+            string action = layerActivations[i];
+            switch (action)
+            {
+                case "sigmoid":
+                    activations[i] = Activation.Sigmoid;
+                    break;
+                case "tanh":
+                    activations[i] = Activation.Tanh;
+                    break;
+                case "relu":
+                    activations[i] = Activation.ReLU;
+                    break;
+                case "leakyRelu":
+                    activations[i] = Activation.LeakyReLU;
+                    break;
+                default:
+                    activations[i] = Activation.ReLU;
+                    break;
+            }
+        }
+
+        InitNeurons();
+        InitBiases();
+        InitWeights();
+    }
+
+    //create empty storage array for the neurons in the network
+    private void InitNeurons()
+    {
+        List<float[]> neuronsList = new List<float[]>();
+        for (int i = 0; i < layers.Length; i++)
+        {
+            neuronsList.Add(new float[layers[i]]);
+        }
+        neurons = neuronsList.ToArray();
+    }
+
+    //initializes random array for the biases being held within the network
+    private void InitBiases()
+    {
+        List<float[]> biasList = new List<float[]>();
+        for (int i = 1; i < layers.Length; i++)
+        {
+            float[] bias = new float[layers[i]];
+            for (int j = 0; j < layers[i]; j++)
+            {
+                bias[j] = RandomHelper.RandomFloat(-0.5f, 0.5f, 0);
+            }
+            biasList.Add(bias);
+        }
+        biases = biasList.ToArray();
+    }
+
+    // initializes random array for the weights being held in the network
+    private void InitWeights()
+    {
+        List<float[][]> weightsList = new List<float[][]>();
+        for (int i = 1; i < layers.Length; i++)
+        {
+            List<float[]> layerWeightsList = new List<float[]>();
+            int neuronsInPreviousLayer = layers[i - 1];
+            for (int j = 0; j < layers[i]; j++)
+            {
+                float[] neuronWeights = new float[neuronsInPreviousLayer];
+                for (int k = 0; k < neuronsInPreviousLayer; k++)
+                {
+                    neuronWeights[k] = RandomHelper.RandomFloat(-0.5f, 0.5f, 0);
+                }
+                layerWeightsList.Add(neuronWeights);
+            }
+            weightsList.Add(layerWeightsList.ToArray());
+        }
+        weights = weightsList.ToArray();
+    }
+
+    // feed forward, inputs >==> outputs
+    public float[] FeedForward(float[] inputs)
+    {
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            neurons[0][i] = inputs[i];
+        }
+        for (int i = 1; i < layers.Length; i++)
+        {
+            int layer = i - 1;
+            for (int j = 0; j < layers[i]; j++)
+            {
+                float value = 0f;
+                for (int k = 0; k < layers[i - 1]; k++)
+                {
+                    value += weights[i - 1][j][k] * neurons[i - 1][k];
+                }
+                neurons[i][j] = Backpropagate.activate(this, value + biases[i-1][j], layer);
+            }
+        }
+        return neurons[layers.Length-1];
+    }
+
+    //Genetic implementations down onwards until save.
+
+    public void Mutate(int high, float val)//used as a simple mutation function for any genetic implementations.
+    {
+        for (int i = 0; i < biases.Length; i++)
+        {
+            for (int j = 0; j < biases[i].Length; j++)
+            {
+                biases[i][j] = (UnityEngine.Random.Range(0f, high) <= 2) ? biases[i][j] += UnityEngine.Random.Range(-val, val) : biases[i][j];
+            }
+        }
+
+        for (int i = 0; i < weights.Length; i++)
+        {
+            for (int j = 0; j < weights[i].Length; j++)
+            {
+                for (int k = 0; k < weights[i][j].Length; k++)
+                {
+                    weights[i][j][k] = (UnityEngine.Random.Range(0f, high) <= 2) ? weights[i][j][k] += UnityEngine.Random.Range(-val, val) : weights[i][j][k];
+                }
+            }
+        }
+    }
+
+    public int CompareTo(NeuralNetwork other) //Comparing For Genetic implementations. Used for sorting based on the fitness of the network
+    {
+        if (other == null) return 1;
+
+        if (fitness > other.fitness)
+            return 1;
+        else if (fitness < other.fitness)
+            return -1;
+        else
+            return 0;
+    }
+
+    // For creating a deep copy, to ensure arrays are serialized
+    public NeuralNetwork Copy(NeuralNetwork nn) 
+    {
+        for (int i = 0; i < biases.Length; i++)
+        {
+            for (int j = 0; j < biases[i].Length; j++)
+            {
+                nn.biases[i][j] = biases[i][j];
+            }
+        }
+        for (int i = 0; i < weights.Length; i++)
+        {
+            for (int j = 0; j < weights[i].Length; j++)
+            {
+                for (int k = 0; k < weights[i][j].Length; k++)
+                {
+                    nn.weights[i][j][k] = weights[i][j][k];
+                }
+            }
+        }
+        return nn;
+    }
+
+    // save and load functions
+    // this loads the biases and weights from within a file into the neural network
+    public void Load(string path)
+    {
+        TextReader tr = new StreamReader(path);
+        int NumberOfLines = (int)new FileInfo(path).Length;
+        string[] ListLines = new string[NumberOfLines];
+        int index = 1;
+        for (int i = 1; i < NumberOfLines; i++)
+        {
+            ListLines[i] = tr.ReadLine();
+        }
+        tr.Close();
+        if (new FileInfo(path).Length > 0)
+        {
+            for (int i = 0; i < biases.Length; i++)
+            {
+                for (int j = 0; j < biases[i].Length; j++)
+                {
+                    biases[i][j] = float.Parse(ListLines[index]);
+                    index++;
+                }
+            }
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                for (int j = 0; j < weights[i].Length; j++)
+                {
+                    for (int k = 0; k < weights[i][j].Length; k++)
+                    {
+                        weights[i][j][k] = float.Parse(ListLines[index]); ;
+                        index++;
+                    }
+                }
+            }
+        }
+    }
+
+    // this is used for saving the biases and weights within the network to a file
+    public void Save(string path)
+    {
+        File.Create(path).Close();
+        StreamWriter writer = new StreamWriter(path, true);
+
+        for (int i = 0; i < biases.Length; i++)
+        {
+            for (int j = 0; j < biases[i].Length; j++)
+            {
+                writer.WriteLine(biases[i][j]);
+            }
+        }
+
+        for (int i = 0; i < weights.Length; i++)
+        {
+            for (int j = 0; j < weights[i].Length; j++)
+            {
+                for (int k = 0; k < weights[i][j].Length; k++)
+                {
+                    writer.WriteLine(weights[i][j][k]);
+                }
+            }
+        }
+        writer.Close();
+    }
+}
