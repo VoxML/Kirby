0ed8639058c8530a4b36f9adc25ec9c0abea68ad
nkrishnaswamy <nkrishn87@gmail.com>
2020-06-02 15:15:43 -0400

    Add FiducialUpdate

--- Assets/Scripts/JsonUpdates/FiducialUpdate.cs
+++ Assets/Scripts/JsonUpdates/FiducialUpdate.cs
@@ -6,31 +6,65 @@ using Newtonsoft.Json;
 /// This class represents updates to the fiducials (objects of interest) received from Kirby
 /// </summary>
 
+/*
+ *  {
+ *      "fid_count": 1,
+ *      "frame": "odom",
+ *      "dict": 7,
+ *      "data": [{
+ *          "pose": {
+ *              "location": [0.5841248162492049, 0.001350950778304289, 0.1153394747832589],
+ *              "orientation": [1.5614911244084855, -0.00026252152137503856, -1.5692386470299342]
+ *          }, 
+ *          "fid": "1"
+ *      }]
+ *  }  
+ */
+
+public class FiducialPose
+{
+    [JsonProperty("location")]
+    public List<float> location { get; set; }
+
+    [JsonProperty("orientation")]
+    public List<float> orientation { get; set; }
+}
+
+public class FiducialData
+{
+    [JsonProperty("pose")]
+    public FiducialPose pose { get; set; }
+
+    [JsonProperty("fid")]
+    public int fid { get; set; }
+
+    public FiducialData()
+    {
+        pose = new FiducialPose();
+        fid = -1;
+    }
+}
 
 public class FiducialUpdate
 {
-    [JsonProperty("id")]
-    public int id { get; set; }
+    [JsonProperty("fid_count")]
+    public int fidCount { get; set; }
 
-    [JsonProperty("width")]
-    public int width { get; set; }
+    [JsonProperty("frame")]
+    public string frame { get; set; }
 
-    [JsonProperty("height")]
-    public int height { get; set; }
-
-    [JsonProperty("resolution")]
-    public float resolution { get; set; }
+    [JsonProperty("dict")]
+    public int dict { get; set; }
 
     [JsonProperty("data")]
-    public List<List<int>> data { get; set; }
+    public List<FiducialData> data { get; set; }
 
     public FiducialUpdate()
     {
-        id = -1;
-        width = 0;
-        height = 0;
-        resolution = 0;
-        data = new List<List<int>>();
+        fidCount = -1;
+        frame = string.Empty;
+        dict = -1;
+        data = new List<FiducialData>();
     }
 }
 
