e62f1077f9d0845403ebb0e7a730e90058d905ef
nkrishnaswamy <nkrishn87@gmail.com>
2020-06-12 23:27:45 -0400

    Add fiducial orientation, adjustment on fiducials that already exist

--- Assets/Scripts/Updaters/FiducialUpdater.cs
+++ Assets/Scripts/Updaters/FiducialUpdater.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -24,29 +25,69 @@ public class FiducialUpdater : MonoBehaviour
     {
         for (int i = 0; i < update.fidCount; i++)
         {
-            GameObject fidObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            fidObj.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-
-            Vector3 coords = new Vector3(-update.data[i].pose.location[1],
-                update.data[i].pose.location[2], update.data[i].pose.location[0]);
-
-            if (update.frame == "odom")
+            GameObject fidObj = null;
+            try
             {
-                fidObj.transform.position = coords;
+                fidObj = fiducials.transform.Find(string.Format("Fiducial_{0}", update.data[i].fid)).gameObject;
             }
-            else if (update.frame == "camera")
+            catch (Exception ex)
             {
-
+                if (ex is NullReferenceException)
+                {
+                    Debug.Log(string.Format("No fiducial object with ID {0} found.  Creating new fiducial object.", i));
+                }
             }
 
-            if (presetMaterials.Count > i)
+            if (fidObj != null)
             {
-                fidObj.GetComponent<Renderer>().material = presetMaterials[i];
+                // fiducial object with this ID already exists
+                Vector3 coords = new Vector3(-update.data[i].pose.location[1],
+                    update.data[i].pose.location[2], update.data[i].pose.location[0]);
+
+                Vector3 rot = new Vector3(update.data[i].pose.orientation[1] * Mathf.Rad2Deg,
+                    update.data[i].pose.location[0] * Mathf.Rad2Deg, update.data[i].pose.location[2] * Mathf.Rad2Deg);
+
+                if (update.frame == "odom")
+                {
+                    fidObj.transform.position = coords;
+                }
+                else if (update.frame == "camera")
+                {
+
+                }
+                fidObj.transform.eulerAngles = rot;
             }
+            else
+            {
+                // create a new fiducial object
+                fidObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                fidObj.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
 
-            fidObj.name = string.Format("Fiducial_{0}", update.data[i].fid);
+                Vector3 coords = new Vector3(-update.data[i].pose.location[1],
+                    update.data[i].pose.location[2], update.data[i].pose.location[0]);
 
-            fidObj.transform.parent = fiducials.transform;
+                Vector3 rot = new Vector3(update.data[i].pose.orientation[1] * Mathf.Rad2Deg,
+                    update.data[i].pose.location[0] * Mathf.Rad2Deg, update.data[i].pose.location[2] * Mathf.Rad2Deg);
+
+                if (update.frame == "odom")
+                {
+                    fidObj.transform.position = coords;
+                }
+                else if (update.frame == "camera")
+                {
+
+                }
+                fidObj.transform.eulerAngles = rot;
+
+                if (presetMaterials.Count > i)
+                {
+                    fidObj.GetComponent<Renderer>().material = presetMaterials[i];
+                }
+
+                fidObj.name = string.Format("Fiducial_{0}", update.data[i].fid);
+
+                fidObj.transform.parent = fiducials.transform;
+            }
         }
     }
 }
