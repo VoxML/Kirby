ca84554088013c5780c0e04585317a3fd86b51cd
nkrishnaswamy <nkrishn87@gmail.com>
2020-08-18 11:52:56 -0600

    Add TODOs

--- Assets/Scripts/Modules/KirbyPredicatesModule.cs
+++ Assets/Scripts/Modules/KirbyPredicatesModule.cs
@@ -1,12 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using VoxSimPlatform.Core;
+
 public class KirbyPredicatesModule : ModuleBase
 {
+    EventManager eventManager;
+
     // Use this for initialization
     void Start()
     {
-
+        // TODO: get game object named "BehaviorController"
+        // TODO: set eventManager = EventManager component on BehaviorController
+        // TODO: eventManager.NonexistentEntityError += NameOfMethodToTriggerFind
     }
 
     // Update is called once per frame
@@ -19,4 +25,7 @@ public class KirbyPredicatesModule : ModuleBase
     {
 
     }
+
+    // TODO: Add NameOfMethodToTriggerFind event handler
+    // see example: https://docs.microsoft.com/en-us/dotnet/api/system.eventhandler?view=netcore-3.1
 }
