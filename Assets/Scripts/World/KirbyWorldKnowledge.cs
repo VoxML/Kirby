fceaf98a44af4d88130dfad3d48f5c17ac5bc216
nkrishnaswamy <nkrishn87@gmail.com>
2020-08-27 15:56:29 -0600

    New TODO

--- Assets/Scripts/World/KirbyWorldKnowledge.cs
+++ Assets/Scripts/World/KirbyWorldKnowledge.cs
@@ -11,7 +11,7 @@ public class KirbyWorldKnowledge : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // TODO: instantiate object dict (new Dictionary<,>)
     }
 
     // Update is called once per frame
